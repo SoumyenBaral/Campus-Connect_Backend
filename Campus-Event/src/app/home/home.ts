f05fc2c96fba596e218897a0662125a4e3ad0c21
Nisha-div635 <patranisha462@gmail.com>
2025-08-25 09:57:51 +0530

    star rating update

--- Campus-Event/src/app/home/home.ts
+++ Campus-Event/src/app/home/home.ts
@@ -2,11 +2,10 @@
 import { Component} from '@angular/core';
 import { Herosection } from "../herosection/herosection";
 import { Footer } from "../footer/footer";
-import { Feedback } from '../feedback/feedback';
 
 @Component({
   selector: 'app-home',
-  imports:[Herosection, Footer,Feedback],
+  imports:[Herosection, Footer],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
--- Campus-Event/src/app/student/student.ts
+++ Campus-Event/src/app/student/student.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { Footer } from '../footer/footer';
+import { Feedback } from '../feedback/feedback';
 
 @Component({
   selector: 'app-student',
-  imports: [Footer],
+  imports: [Footer,Feedback],
   templateUrl: './student.html',
   styleUrl: './student.css'
 })
