1f9dbf011002baac747185aafbd79d4fcbccf26e
Nisha-div635 <patranisha462@gmail.com>
2025-08-26 11:18:43 +0530

    update

--- Campus-Event/src/app/home/home.ts
+++ Campus-Event/src/app/home/home.ts
@@ -2,11 +2,12 @@
 import { Component} from '@angular/core';
 import { Herosection } from "../herosection/herosection";
 import { Footer } from "../footer/footer";
-import { Gallery } from "../gallery/gallery";
+import { Feedback } from "../feedback/feedback";
+import { HomeGallery } from "../home-gallery/home-gallery";
 
 @Component({
   selector: 'app-home',
-  imports: [Herosection, Footer, Gallery],
+  imports: [Herosection, Footer,Feedback, HomeGallery],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
