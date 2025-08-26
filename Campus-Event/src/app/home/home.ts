b9a1a1eecd78015799b961a0da82c8172064e60a
Soumyen <soumyenbaral620@gmail.com>
2025-08-26 13:19:53 +0530

    home complete

--- Campus-Event/src/app/home/home.ts
+++ Campus-Event/src/app/home/home.ts
@@ -1,12 +1,11 @@
 
 import { Component} from '@angular/core';
-import { Herosection } from "../herosection/herosection";
 import { Footer } from "../footer/footer";
 import { Gallery } from "../gallery/gallery";
 
 @Component({
   selector: 'app-home',
-  imports: [Herosection, Footer, Gallery],
+  imports: [Footer],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
