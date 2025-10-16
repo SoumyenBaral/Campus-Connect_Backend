3b76096a9302b08b8cc05514083bd9feaa382262
Soumyen <soumyenbaral620@gmail.com>
2025-10-17 00:39:59 +0530

    invitation component work done

--- Campus-Event/src/app/home/home.ts
+++ Campus-Event/src/app/home/home.ts
@@ -2,16 +2,13 @@
 import { Component} from '@angular/core';
 import { Footer } from "../footer/footer";
 import { Navbar } from "../navbar/navbar";
-import { RegisterPage } from "../register-page/register-page";
-import { Signup } from '../signup/signup';
 import { CreateEvent } from '../create-event/create-event';
-import { Invitation } from '../invitation/invitation';
 
 
 
 @Component({
   selector: 'app-home',
-  imports: [Footer, Navbar,CreateEvent,Invitation],
+  imports: [Footer, Navbar],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
