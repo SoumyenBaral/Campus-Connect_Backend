1cd694f7ead8268a1364227d5a45d0bfb9cb3287
Soumyen <soumyenbaral620@gmail.com>
2025-11-01 02:00:01 +0530

    dynamically changes works

---
+++ src/main/java/com/campus/connect/Entity/index.java
@@ -0,0 +1,25 @@
+package com.campus.connect.Entity;
+
+public class index {
+int id;
+String name;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+@Override
+public String toString() {
+	return "index [id=" + id + ", name=" + name + "]";
+}
+
+
+
+}
