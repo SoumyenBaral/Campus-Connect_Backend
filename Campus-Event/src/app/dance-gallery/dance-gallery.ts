93f4d8aa2289d3ba7ca6e9c2bf90e6e123fc1746
Nisha-div635 <patranisha462@gmail.com>
2026-02-11 22:44:42 +0530

    Otp partially done

--- Campus-Event/src/app/dance-gallery/dance-gallery.ts
+++ Campus-Event/src/app/dance-gallery/dance-gallery.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { Navbar } from '../navbar/navbar';
+import { Footer } from '../footer/footer';
 
 @Component({
   selector: 'app-dance-gallery',
-  imports: [RouterLink,RouterOutlet],
+  imports: [RouterLink,RouterOutlet,Navbar,Footer],
   templateUrl: './dance-gallery.html',
   styleUrl: './dance-gallery.css'
 })
--- Campus-Event/src/app/home-gallery/home-gallery.ts
+++ Campus-Event/src/app/home-gallery/home-gallery.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { Navbar } from "../navbar/navbar";
-import { Navbar } from '../navbar/navbar';
 import { CommonModule } from '@angular/common';
+import { Footer } from '../footer/footer';
 
 @Component({
   selector: 'app-home-gallery',
   standalone: true,
-  imports: [RouterLink,RouterOutlet,Navbar , CommonModule],
+  imports: [RouterLink,RouterOutlet,Navbar, CommonModule,Footer],
   templateUrl: './home-gallery.html',
   styleUrl: './home-gallery.css'
 })
