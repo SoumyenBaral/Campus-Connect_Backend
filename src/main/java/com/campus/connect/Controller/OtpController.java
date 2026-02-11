632897ec5d0f7f0652e3f8078975ea500bc552f2
Nisha-div635 <patranisha462@gmail.com>
2026-02-11 22:44:09 +0530

    Otp  function updated

--- src/main/java/com/campus/connect/Controller/OtpController.java
+++ src/main/java/com/campus/connect/Controller/OtpController.java
@@ -2,13 +2,14 @@ package com.campus.connect.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.campus.connect.Service.OtpService;
 
-
+@CrossOrigin(origins="http://localhost:4200/")
 @RestController
 @RequestMapping("/api")
 public class OtpController {
