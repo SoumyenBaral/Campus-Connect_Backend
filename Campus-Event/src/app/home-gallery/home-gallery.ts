4cd5b2fb80cbcf41d6787f99cb232a6654b03605
Soumyen <soumyenbaral620@gmail.com>
2026-02-12 22:02:59 +0530

    loading....

--- Campus-Event/src/app/home-gallery/home-gallery.ts
+++ Campus-Event/src/app/home-gallery/home-gallery.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { Navbar } from "../navbar/navbar";
 import { Navbar } from '../navbar/navbar';
 import { CommonModule } from '@angular/common';
 
